cc6875c5b787117e14172b9f838422becbb01a30
lijinze <2533755010@qq.com>
2021-01-18 00:36:24 +0800

    fix mybatis-plus-gen

--- mybatis-plus-gen/src/main/java/com/creaway/controller/AppAlarmController.java
+++ mybatis-plus-gen/src/main/java/com/creaway/controller/AppAlarmController.java
@@ -1,9 +1,15 @@
 package com.creaway.controller;
 
 
+import com.creaway.model.AppAlarm;
+import com.creaway.service.AppAlarmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/appAlarm")
 public class AppAlarmController {
+    @Autowired
+    AppAlarmService appAlarmService;
 
+    @GetMapping("/")
+    public void getall() {
+        //        Main.autoGenerator().execute();
+        AppAlarm entity = new AppAlarm();
+        entity.setAlarmDate(LocalDateTime.now());
+        entity.setAlarmLevel("1");
+        entity.setAlarmNo(1);
+        entity.setAlarmStatus("1");
+        entity.setInfo("test");
+        appAlarmService.list();
+    }
 }
--- mybatis-plus-gen/src/test/java/App.java
+++ mybatis-plus-gen/src/test/java/App.java
@@ -1,7 +1,11 @@
 import com.creaway.Main;
 import com.creaway.mapper.AppAlarmDao;
 import com.creaway.model.AppAlarm;
+import com.creaway.service.AppAlarmService;
+
 import org.junit.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
@@ -12,14 +16,14 @@ import java.time.LocalDateTime;
  * @Date: 2021/1/17 23:24
  * @Description:
  */
-@SpringBootTest
+@SpringBootTest(classes = Main.class)
 public class App {
-    @Resource
-    AppAlarmDao appAlarmDao;
+    @Autowired
+    AppAlarmService appAlarmService;
 
     //mybatis-plus逆向工程
     @Test
-    public void testMappingGeneratorOracle() {
+    public void testMappingGenerator() {
 //        Main.autoGenerator().execute();
         AppAlarm entity = new AppAlarm();
         entity.setAlarmDate(LocalDateTime.now());
@@ -27,7 +31,7 @@ public class App {
         entity.setAlarmNo(1);
         entity.setAlarmStatus("1");
         entity.setInfo("test");
-        appAlarmDao.insert(entity);
+        appAlarmService.list();
 
     }
 
